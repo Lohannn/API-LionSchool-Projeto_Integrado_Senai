1f90f7fcd214326695f6a4781a04e7ae918d36a5
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2023-03-27 15:02:24 -0300

    url atualizada dos icones

--- modulo/json/cursos.js
+++ modulo/json/cursos.js
@@ -2,13 +2,13 @@ var cursos = [
     {
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
-        "icone" :   "https://i.imgur.com/7SA6Lf0_d.webp?maxwidth=760&fidelity=grand",
+        "icone" :   "https://i.imgur.com/7SA6Lf0.png",
         "carga" :   "1200",
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
-        "icone" :   "https://i.imgur.com/rRj9rz5_d.webp?maxwidth=760&fidelity=grand",
+        "icone" :   "https://i.imgur.com/rRj9rz5.png",
         "carga" :   "1200"
     }
 ];
